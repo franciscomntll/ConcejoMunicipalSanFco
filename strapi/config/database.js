ec56722d9dd697c4fa17bea7435872ff052593c2
Francisco Montilla <ciscomontilla@gmail.com>
2023-02-04 14:33:57 -0400

    add strapi

---
+++ strapi/config/database.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'concejo'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'admin'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: env('DATABASE_SSL', false)
+    },
+    debug: false,
+
+  },
+});
